89edb474863cf1db2d4687da4a96d322824d6838
Peter <52176852+Peter537@users.noreply.github.com>
2025-11-28 20:56:08 +0100

    feat(order): add order controller and dto's

---
+++ MToGo/src/MToGo.OrderService/Controllers/OrdersController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using MToGo.OrderService.Logging;
+using MToGo.OrderService.Models;
+using MToGo.OrderService.Services;
+
+namespace MToGo.OrderService.Controllers
+{
+    [ApiController]
+    [Route("orders")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        private readonly ILogger<OrdersController> _logger;
+
+        public OrdersController(IOrderService orderService, ILogger<OrdersController> logger)
+        {
+            _orderService = orderService;
+            _logger = logger;
+        }
+
+        [HttpPost("order")]
+        [ProducesResponseType(typeof(OrderCreateResponse), 201)]
+        public async Task<IActionResult> CreateOrder(OrderCreateRequest request)
+        {
+            _logger.ReceivedCreateOrderRequest();
+
+            var response = await _orderService.CreateOrderAsync(request);
+
+            // Audit log
+            _logger.CreateOrderCompleted(response.Id);
+
+            return StatusCode(201, response);
+        }
+    }
+}
