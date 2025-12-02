d10986e9eb02dbdbaf5d343d067eccf3881e1db0
Peter <52176852+Peter537@users.noreply.github.com>
2025-12-02 18:12:33 +0100

    feat(partner): add menu service methods

--- MToGo/src/MToGo.PartnerService/Services/IPartnerService.cs
+++ MToGo/src/MToGo.PartnerService/Services/IPartnerService.cs
@@ -6,4 +6,8 @@ public interface IPartnerService
 {
     Task<CreatePartnerResponse> RegisterPartnerAsync(PartnerRegisterRequest request);
     Task<PartnerLoginResponse> LoginAsync(PartnerLoginRequest request);
+    Task<PartnerDetailsResponse?> GetPartnerByIdAsync(int partnerId);
+    Task<CreateMenuItemResponse> AddMenuItemAsync(int partnerId, CreateMenuItemRequest request);
+    Task UpdateMenuItemAsync(int partnerId, int menuItemId, UpdateMenuItemRequest request);
+    Task DeleteMenuItemAsync(int partnerId, int menuItemId);
 }
