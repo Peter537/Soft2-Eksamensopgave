07d953ec16a886af8d30d4a8f47662752b9c3361
Peter <52176852+Peter537@users.noreply.github.com>
2025-12-05 18:03:24 +0100

    refactor(kafka): moved interfaces to impls

--- MToGo/src/MToGo.Shared/Kafka/IKafkaProducer.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace MToGo.Shared.Kafka
-{
-    public interface IKafkaProducer
-    {
-        Task PublishAsync<T>(string topic, string key, T eventData);
-    }
-}
