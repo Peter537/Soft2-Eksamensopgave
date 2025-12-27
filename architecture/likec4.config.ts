2a3265d7037b6a26b26c4c413bc75d48ac1f6af0
Peter <52176852+Peter537@users.noreply.github.com>
2025-12-27 22:00:00 +0100

    feat(architecture): add likec4 architecture as code

---
+++ architecture/likec4.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'likec4/config'
+
+export default defineConfig({
+  name: 'mtogo-architecture',
+  title: 'MToGo Architecture',
+  exclude: ['**/node_modules/**', '**/dist/**', '**/.cache/**'],
+})
